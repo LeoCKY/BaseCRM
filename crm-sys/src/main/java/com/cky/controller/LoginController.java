1ed726a0d5f2432f84bcd11240319360caa80b27
Leo Chen <tbdsyu9527@gmail.com>
2020-08-04 00:51:27 +0800

    init

---
+++ crm-sys/src/main/java/com/cky/controller/LoginController.java
@@ -0,0 +1,68 @@
+package com.cky.controller;
+
+import com.cky.filter.VerifyCodeUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Controller
+@Slf4j
+public class LoginController {
+
+
+    @GetMapping(value = "")
+    public String loginInit() {
+        return loginCheck();
+    }
+
+    @GetMapping(value = "goLogin")
+    public String goLogin(Model model) {
+        Subject sub = SecurityUtils.getSubject();
+        if (sub.isAuthenticated()) {
+            return "/main/main";
+        } else {
+            model.addAttribute("message", "请重新登录");
+            return "/login";
+        }
+    }
+
+    @GetMapping(value = "/login")
+    public String loginCheck() {
+        Subject sub = SecurityUtils.getSubject();
+        Boolean flag2 = sub.isRemembered();
+        boolean flag = sub.isAuthenticated() || flag2;
+        if (flag) {
+            return "/main/main";
+        }
+        return "/login";
+    }
+
+    @GetMapping(value = "/getCode")
+    public void getYzm(HttpServletResponse response, HttpServletRequest request) {
+        try {
+            response.setHeader("Pragma", "No-cache");
+            response.setHeader("Cache-Control", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setContentType("image/jpg");
+
+            //生成随机字串
+            String verifyCode = VerifyCodeUtils.generateVerifyCode(4);
+            log.info("verifyCode:{}", verifyCode);
+            //存入会话session
+            HttpSession session = request.getSession(true);
+            session.setAttribute("_code", verifyCode.toLowerCase());
+            //生成图片
+            int w = 146, h = 33;
+            VerifyCodeUtils.outputImage(w, h, response.getOutputStream(), verifyCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ crm-sys/src/main/java/com/cky/shiro/Principal.java
@@ -0,0 +1,39 @@
+package com.cky.shiro;
+
+import com.cky.base.user.CurrentUser;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.Subject;
+
+
+public class Principal {
+
+    /**
+     * 获取用户主题
+     * @return
+     */
+    public static Subject getSubject() {
+        return SecurityUtils.getSubject();
+    }
+
+    /**
+     * 获取当前用户对象
+     * @return
+     */
+    public static CurrentUser getPrincipal() {
+        return (CurrentUser) getSubject().getPrincipal();
+    }
+
+    /**
+     * 当前session
+     * @return
+     */
+    public static Session getSession() {
+        return getSubject().getSession();
+    }
+
+    public static CurrentUser getCurrentUse() {
+        return (CurrentUser) getSession().getAttribute("currentPrincipal");
+    }
+
+}
